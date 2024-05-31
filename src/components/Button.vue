1c28bdd3ca7aa9cb5b6e1a492c36446b38e7b682
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-31 23:43:44 +0600

    resolve "src/components/Button.vue(15,106): error TS2345: Argument of type 'unknown' is not assignable to parameter of type 'ClassValue'.  ELIFECYCLE  Command failed with exit code 2. Error: Command "pnpm run build" exited with 2"

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,18 +1,27 @@
 <script lang="ts" setup>
 import { useAttrs } from 'vue'
 import { cn } from '../lib/utils'
+import { type ClassValue } from "clsx"
 
 const attrs = useAttrs()
-// console.log(attrs.class)
 </script>
 
 <template>
-    <button v-motion :initial="{ scale: 0 }" :enter="{
-        scale: 1, transition: {
-            type: 'spring',
-        }
-    }" :duration="600"
-        :class="cn('bg-gradient-to-tr from-primary to-secondary text-white font-bold py-2 px-4 rounded', attrs.class)">
-        <slot />
-    </button>
+  <button
+    v-motion
+    :initial="{ scale: 0 }"
+    :enter="{
+      scale: 1,
+      transition: {
+        type: 'spring'
+      }
+    }"
+    :duration="600"
+    :class="cn(
+      'bg-gradient-to-tr from-primary to-secondary text-white font-bold py-2 px-4 rounded',
+      attrs.class ? (attrs.class as ClassValue[]) : []
+    )"
+  >
+    <slot />
+  </button>
 </template>
