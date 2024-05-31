de2bdf66b71d06906e5e98c12c7d69f2f51d924e
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-31 22:04:10 +0600

    config @vueuse/motion

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
+import { MotionPlugin } from '@vueuse/motion'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(MotionPlugin)
+
+app.mount('#app')
