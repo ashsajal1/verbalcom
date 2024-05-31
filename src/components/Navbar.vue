0ad0b81c28d3d8133a1461372a336e5855c0a4ca
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-31 23:00:27 +0600

    add backdrop when scrolly is greater than 100

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,37 @@
 <script lang="ts" setup>
+import { ref, onMounted, onUnmounted } from 'vue'
 
+const isScrolled = ref(false)
+
+const handleScroll = () => {
+    isScrolled.value = window.scrollY > 100
+    // console.log(isScrolled.value)
+}
+
+onMounted(() => {
+    window.addEventListener('scroll', handleScroll)
+})
+
+onUnmounted(() => {
+    window.removeEventListener('scroll', handleScroll)
+})
 </script>
 
 <template>
-    <header class="flex items-center justify-between p-4 h-[80px] bg-white w-full z-10 border-b fixed top-0">
+    <header
+        class="flex items-center justify-between p-4 h-[80px] bg-white w-full z-10 border-b fixed top-0 transition-all duration-300"
+        :class="{ 'backdrop-blur bg-opacity-60': isScrolled }">
         <p class="text-3xl font-bold bg-gradient-to-tr from-primary to-secondary bg-clip-text text-transparent">
-            VerbalCom</p>
+            VerbalCom
+        </p>
         <div>
             <div class="focus-within:bg-gradient-to-tr from-primary to-secondary rounded p-[1px]">
                 <div class="border bg-white rounded p-2 flex items-center">
-                    <input class="outline-none" type="text" placeholder="Search sample...">
+                    <input class="outline-none" type="text" placeholder="Search sample..." />
                     <button
-                        class="rounded bg-gradient-to-tr from-primary to-secondary p-1 text-sm text-white text-center flex items-center justify-between">Search</button>
+                        class="rounded bg-gradient-to-tr from-primary to-secondary p-1 text-sm text-white text-center flex items-center justify-between">
+                        Search
+                    </button>
                 </div>
             </div>
         </div>
