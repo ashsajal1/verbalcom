9230daa1660e9675fb85734e9627c940258cc53e
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-31 22:34:26 +0600

    add padding

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,18 @@ import { communicationSamples } from './lib/texts'
     <div>
       <div class="border rounded p-2 flex items-center focus-within:border focus-within:border-teal-600">
         <input class="outline-none" type="text" placeholder="Search sample...">
-        <button class="rounded bg-teal-600 p-1 text-sm text-white text-center flex items-center justify-between">Search</button>
+        <button
+          class="rounded bg-teal-600 p-1 text-sm text-white text-center flex items-center justify-between">Search</button>
       </div>
     </div>
   </header>
-  <div v-for="sample in communicationSamples" :key="sample.text">
-    <div v-motion-roll-visible-once-top :duration="600" class="flex flex-col border shadow p-4 rounded my-4">
-      <p class="font-extralight text-2xl">{{ sample.text }}</p>
-      <p class="font-bold text-sm">{{ sample.category }}</p>
+
+  <main class="p-4">
+    <div v-for="sample in communicationSamples" :key="sample.text">
+      <div v-motion-roll-top :duration="600" class="flex flex-col border shadow p-4 rounded my-4">
+        <p class="font-extralight text-2xl">{{ sample.text }}</p>
+        <p class="font-bold text-sm">{{ sample.category }}</p>
+      </div>
     </div>
-  </div>
+  </main>
 </template>
