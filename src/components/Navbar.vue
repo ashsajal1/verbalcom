42cd0648ff7c686c08e95152bf18ba8a2503ad41
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-31 22:46:19 +0600

    add gradinet border instead teal border

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,12 +4,15 @@
 
 <template>
     <header class="flex items-center justify-between p-4">
-        <p class="text-3xl font-bold bg-gradient-to-tr from-primary to-secondary bg-clip-text text-transparent">VerbalCom</p>
+        <p class="text-3xl font-bold bg-gradient-to-tr from-primary to-secondary bg-clip-text text-transparent">
+            VerbalCom</p>
         <div>
-            <div class="border rounded p-2 flex items-center focus-within:border focus-within:border-teal-600">
-                <input class="outline-none" type="text" placeholder="Search sample...">
-                <button
-                    class="rounded bg-gradient-to-tr from-primary to-secondary p-1 text-sm text-white text-center flex items-center justify-between">Search</button>
+            <div class="focus-within:bg-gradient-to-tr from-primary to-secondary rounded p-[1px]">
+                <div class="border bg-white rounded p-2 flex items-center">
+                    <input class="outline-none" type="text" placeholder="Search sample...">
+                    <button
+                        class="rounded bg-gradient-to-tr from-primary to-secondary p-1 text-sm text-white text-center flex items-center justify-between">Search</button>
+                </div>
             </div>
         </div>
     </header>
