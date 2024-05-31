d0dca3a9c1673b4db3d4363e0bac10a9ac6d69b2
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-31 23:31:17 +0600

    remove log and unnecessary condition

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -3,7 +3,7 @@ import { useAttrs } from 'vue'
 import { cn } from '../lib/utils'
 
 const attrs = useAttrs()
-console.log(attrs.class)
+// console.log(attrs.class)
 </script>
 
 <template>
@@ -12,7 +12,7 @@ console.log(attrs.class)
             type: 'spring',
         }
     }" :duration="600"
-        :class="cn('bg-gradient-to-tr from-primary to-secondary text-white font-bold py-2 px-4 rounded', attrs.class ? [attrs.class] : [])">
+        :class="cn('bg-gradient-to-tr from-primary to-secondary text-white font-bold py-2 px-4 rounded', attrs.class)">
         <slot />
     </button>
 </template>
