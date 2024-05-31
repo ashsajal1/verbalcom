015f6904f0df5c710ea144584ecaaed7ebffeb34
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-31 22:07:39 +0600

    import style

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,6 @@
 </script>
 
 <template>
-  <div class="bg-blue-700">Verbal Communication app</div>
+  <div v-motion :duration="700" :initial="{ opacity: 0, x: -1000 }" :enter="{ opacity: 1, x: 0 }" class="bg-blue-700">
+    Verbal Communication app</div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { MotionPlugin } from '@vueuse/motion'
 import App from './App.vue'
+import './style.css'
 
 const app = createApp(App)
 
