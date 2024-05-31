28be45b11e5c080921cb029778a8446fbc5cc90b
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-31 23:14:40 +0600

    add animaton to button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -5,7 +5,12 @@ const attrs = useAttrs()
 </script>
 
 <template>
-  <button v-bind="attrs" class="bg-gradient-to-tr from-primary to-secondary text-white font-bold py-2 px-4 rounded">
-    <slot />
-  </button>
+    <button v-motion :initial="{ scale: 0 }" :enter="{
+        scale: 1, transiton: {
+            type: 'spring',
+        }
+    }" :duration="600" v-bind="attrs"
+        class="bg-gradient-to-tr from-primary to-secondary text-white font-bold py-2 px-4 rounded">
+        <slot />
+    </button>
 </template>
