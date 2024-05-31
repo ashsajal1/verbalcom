b15ecd6b684d2b30b061a9533f9902748fb06e14
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-31 22:40:59 +0600

    add gradient text and background

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,12 +4,12 @@
 
 <template>
     <header class="flex items-center justify-between p-4">
-        <p class="text-3xl font-bold">VerbalCom</p>
+        <p class="text-3xl font-bold bg-gradient-to-tr from-teal-600 to-violet-600 bg-clip-text text-transparent">VerbalCom</p>
         <div>
             <div class="border rounded p-2 flex items-center focus-within:border focus-within:border-teal-600">
                 <input class="outline-none" type="text" placeholder="Search sample...">
                 <button
-                    class="rounded bg-teal-600 p-1 text-sm text-white text-center flex items-center justify-between">Search</button>
+                    class="rounded bg-gradient-to-tr from-teal-600 to-violet-600 p-1 text-sm text-white text-center flex items-center justify-between">Search</button>
             </div>
         </div>
     </header>
