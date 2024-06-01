e26a4c24c09b5944d46bc66499e3a246c5ea79d2
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-01 16:12:40 +0600

    start voice when click to text

--- src/components/TextCard.vue
+++ src/components/TextCard.vue
@@ -1,14 +1,38 @@
 <script lang="ts" setup>
+import { useSpeechSynthesis } from '@vueuse/core'
 import { defineProps } from 'vue'
-import Button from '../components/Button.vue';
+import Button from '../components/Button.vue'
 
-defineProps<{ text: string, category: string }>()
+const props = defineProps<{ text: string; category: string }>()
+
+const {
+  isSupported,
+  isPlaying,
+  // status,
+  // voiceInfo,
+  // utterance,
+  // error,
+  stop,
+  speak,
+} = useSpeechSynthesis(props.text)
+
+const handleClick = () => {
+  if (isSupported.value) {
+    if (isPlaying.value) {
+      stop()
+    }
+    speak()
+  } else {
+    console.error('Speech synthesis is not supported in this browser.')
+  }
+}
+
 </script>
 
 <template>
-    <div v-motion-roll-top :duration="600" class="flex flex-col border shadow p-4 rounded my-4">
-        <p class="font-extralight text-2xl">{{ text }}</p>
-        <p class="font-bold text-sm">{{ category }}</p>
-        <Button class="mt-2 hover:to-violet-700 hover:from-teal-700">Practice</Button>
-    </div>
+  <div v-motion-roll-top :duration="600" class="flex flex-col border shadow p-4 rounded my-4">
+    <p @click="handleClick" class="font-extralight text-2xl">{{ text }}</p>
+    <p class="font-bold text-sm">{{ category }}</p>
+    <Button class="mt-2 hover:to-violet-700 hover:from-teal-700">Practice</Button>
+  </div>
 </template>
