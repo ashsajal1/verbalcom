e5b1eb465e9626817960f3376499d8faee9982ac
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-01 12:25:26 +0600

    sejparate text card component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
-import Button from './components/Button.vue';
 import NavbarVue from './components/Navbar.vue'
+import TextCard from './components/TextCard.vue';
 import { communicationSamples } from './lib/texts'
 </script>
 
@@ -9,11 +9,7 @@ import { communicationSamples } from './lib/texts'
 
   <main class="p-4 mt-[80px]">
     <div v-for="sample in communicationSamples" :key="sample.text">
-      <div v-motion-roll-top :duration="600" class="flex flex-col border shadow p-4 rounded my-4">
-        <p class="font-extralight text-2xl">{{ sample.text }}</p>
-        <p class="font-bold text-sm">{{ sample.category }}</p>
-        <Button class="mt-2 hover:to-violet-700 hover:from-teal-700">Practice</Button>
-      </div>
+      <TextCard :text="sample.text" :category="sample.category" />
     </div>
   </main>
 </template>
---
+++ src/components/TextCard.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+import Button from '../components/Button.vue';
+
+defineProps<{ text: string, category: string }>()
+</script>
+
+<template>
+    <div v-motion-roll-top :duration="600" class="flex flex-col border shadow p-4 rounded my-4">
+        <p class="font-extralight text-2xl">{{ text }}</p>
+        <p class="font-bold text-sm">{{ category }}</p>
+        <Button class="mt-2 hover:to-violet-700 hover:from-teal-700">Practice</Button>
+    </div>
+</template>
