afb1f6bbd34ae48d357533e51812d065743396ab
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-02 10:36:33 +0600

    config: add import alaises @ashsajal1 update: get text sample by id

--- src/pages/Practice.vue
+++ src/pages/Practice.vue
@@ -1,11 +1,15 @@
 <script lang="ts" setup>
+import { communicationSamples } from '@/lib/texts';
+import { ref } from 'vue';
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
 const id = route.query.id as string
+const praciceText = ref(communicationSamples.filter(text => text.id === id)[0])
+console.log(praciceText.value)
 </script>
 
 <template>
-  <h1>Practice page:</h1>
-  <p>The ID passed from the previous page is: {{ id }}</p>
+    <h1>Practice page:</h1>
+    <p>The ID passed from the previous page is: {{ id }}</p>
 </template>
