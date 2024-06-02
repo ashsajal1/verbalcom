219bf1f79253b33b954bb5e8a63a76a1b4f1a99e
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-02 12:14:45 +0600

    add: speak listener

--- src/pages/Practice.vue
+++ src/pages/Practice.vue
@@ -25,12 +25,39 @@ const handleClick = () => {
     }
 }
 
+import { useSpeechRecognition } from '@vueuse/core';
+
+const speech = useSpeechRecognition({
+  lang: 'en-US',
+  continuous: true,
+});
+
+const result = ref('');
+
+if (speech.isSupported.value) {
+  speech.result.value = result.value;
+}
+
+function start() {
+  speech.start();
+}
+
+function stopSpeech() {
+  speech.stop();
+}
+
+const { isListening } = speech;
+
 </script>
 
 <template>
     <div>
         <h1 @click="handleClick" class="text-2xl font-extralight py-4">{{ praciceSample.text }}</h1>
-        <textarea placeholder="Write text" class="p-2 rounded border w-full"></textarea>
+        <textarea v-model="result" placeholder="Write text" class="p-2 rounded border w-full"></textarea>
         <Button class="w-full mt-3">Submit</Button>
+        <div class="flex items-center gap-4">
+            <Button v-if="!isListening" @click="start" class="w-full mt-3">Press and talk</Button>
+            <Button v-if="isListening" @click="stopSpeech" class="w-full mt-3">Stop</Button>
+        </div>
     </div>
 </template>
