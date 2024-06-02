fc249772b2aa6986861913698c9946332c46036c
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-02 15:14:20 +0600

    add: icon for speak and stop

--- src/pages/Practice.vue
+++ src/pages/Practice.vue
@@ -53,11 +53,16 @@ const { isListening } = speech;
 
 <template>
     <div>
-        <h1 @click="handleClick" class="text-2xl font-extralight py-4">{{ praciceSample.text }}</h1>
+        <h1 class="text-2xl flex items-center gap-2 font-extralight py-4">
+            <span>{{ praciceSample.text }}</span>
+            <Button variant="ghost">
+                <span v-if="!isPlaying" @click="handleClick" class="pi pi-volume-down"></span>
+                <span v-if="isPlaying" @click="stop()" class="pi pi-volume-off"></span>
+            </Button>
+        </h1>
         <textarea v-model="result" placeholder="Write text" class="p-2 rounded border w-full"></textarea>
         <Button class="w-full mt-3">Submit</Button>
-        <!-- {{ speech.isSupported }}
-        {{ speech.isListening }} -->
+
         <div v-if="!speech.isSupported" class="flex items-center gap-4">
             <Button v-if="!isListening" @click="start" class="w-full mt-3">Press and talk</Button>
             <Button v-if="isListening" @click="stopSpeech" class="w-full mt-3">Stop</Button>
