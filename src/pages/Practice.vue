7bdd34afca42d447197c0cced46327afde8f57da
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-02 10:46:11 +0600

    design: add btn textarea and large text

--- src/pages/Practice.vue
+++ src/pages/Practice.vue
@@ -2,14 +2,17 @@
 import { communicationSamples } from '@/lib/texts';
 import { ref } from 'vue';
 import { useRoute } from 'vue-router'
+import Button from '@/components/Button.vue';
 
 const route = useRoute()
 const id = route.query.id as string
-const praciceText = ref(communicationSamples.filter(text => text.id === id)[0])
-console.log(praciceText.value)
+const praciceSample = ref(communicationSamples.filter(text => text.id === id)[0])
 </script>
 
 <template>
-    <h1>Practice page:</h1>
-    <p>The ID passed from the previous page is: {{ id }}</p>
+    <div>
+        <h1 class="text-2xl font-extralight py-4">{{ praciceSample.text }}</h1>
+        <textarea placeholder="Write text" class="p-2 rounded border w-full"></textarea>
+        <Button class="w-full mt-3">Submit</Button>
+    </div>
 </template>
