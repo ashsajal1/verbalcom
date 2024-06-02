2caf9f6e3f38989cf5fdd932ce169a011cc7bfbe
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-02 12:28:43 +0600

    remove: btn if speech recognition is not supported

--- src/pages/Practice.vue
+++ src/pages/Practice.vue
@@ -28,22 +28,23 @@ const handleClick = () => {
 import { useSpeechRecognition } from '@vueuse/core';
 
 const speech = useSpeechRecognition({
-  lang: 'en-US',
-  continuous: true,
+    lang: 'en-US',
+    continuous: true,
+    interimResults: true
 });
 
 const result = ref('');
 
 if (speech.isSupported.value) {
-  speech.result.value = result.value;
+    speech.result.value = result.value;
 }
 
 function start() {
-  speech.start();
+    speech.start();
 }
 
 function stopSpeech() {
-  speech.stop();
+    speech.stop();
 }
 
 const { isListening } = speech;
@@ -55,7 +56,9 @@ const { isListening } = speech;
         <h1 @click="handleClick" class="text-2xl font-extralight py-4">{{ praciceSample.text }}</h1>
         <textarea v-model="result" placeholder="Write text" class="p-2 rounded border w-full"></textarea>
         <Button class="w-full mt-3">Submit</Button>
-        <div class="flex items-center gap-4">
+        <!-- {{ speech.isSupported }}
+        {{ speech.isListening }} -->
+        <div v-if="!speech.isSupported" class="flex items-center gap-4">
             <Button v-if="!isListening" @click="start" class="w-full mt-3">Press and talk</Button>
             <Button v-if="isListening" @click="stopSpeech" class="w-full mt-3">Stop</Button>
         </div>
