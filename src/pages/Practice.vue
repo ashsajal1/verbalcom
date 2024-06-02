864d214230a08e1075f6ca314f4b347ba669acfa
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-02 10:26:59 +0600

    update: reaceive the id from router

--- src/pages/Practice.vue
+++ src/pages/Practice.vue
@@ -1,7 +1,11 @@
 <script lang="ts" setup>
+import { useRoute } from 'vue-router'
 
+const route = useRoute()
+const id = route.query.id as string
 </script>
 
 <template>
-    <h1>Practice page :</h1>
+  <h1>Practice page:</h1>
+  <p>The ID passed from the previous page is: {{ id }}</p>
 </template>
