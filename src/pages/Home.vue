d45f34e2d6a15d76a89ba99cd698dbd36ef44453
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-02 19:43:39 +0600

    use grid

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,7 +4,9 @@ import { communicationSamples } from '../lib/texts'
 </script>
 
 <template>
-    <div v-for="sample in communicationSamples" :key="sample.id">
-        <TextCard :key="sample.id" :id="sample.id" :text="sample.text" :category="sample.category" />
+    <div class="grid md:grid-cols-2 gap-2">
+        <div v-for="sample in communicationSamples" :key="sample.id">
+            <TextCard :key="sample.id" :id="sample.id" :text="sample.text" :category="sample.category" />
+        </div>
     </div>
 </template>
