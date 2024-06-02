a3f4672087b721297e10a9b2c005b73760cdaaa2
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-02 10:18:07 +0600

    use: id as key

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,7 +4,7 @@ import { communicationSamples } from '../lib/texts'
 </script>
 
 <template>
-    <div v-for="sample in communicationSamples" :key="sample.text">
-        <TextCard :text="sample.text" :category="sample.category" />
+    <div v-for="sample in communicationSamples" :key="sample.id">
+        <TextCard :key="sample.id" :text="sample.text" :category="sample.category" />
     </div>
 </template>
