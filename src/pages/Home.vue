c6f8445d5b5268557121340aa6f61c94b4e26500
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-04 11:52:43 +0600

    add : filter text sample by search query

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,28 @@
 <script lang="ts" setup>
 import TextCard from '../components/TextCard.vue';
-import { communicationSamples } from '../lib/texts'
+import { communicationSamples } from '../lib/texts';
 import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+
 const route = useRoute();
-const searchText = route.query.search
+const searchText = computed(() => route.query.search as string | undefined);
+
+const filteredSamples = computed(() => {
+    if (!searchText.value) return communicationSamples;
+    const searchTerms = searchText.value.toLowerCase().split(' ');
+    return communicationSamples.filter((sample) =>
+        searchTerms.some(
+            (term) =>
+                sample.text.toLowerCase().includes(term) ||
+                sample.category.toLowerCase().includes(term)
+        )
+    );
+});
 </script>
 
 <template>
     <div class="grid md:grid-cols-2 gap-2">
-        <div v-for="sample in communicationSamples" :key="sample.id">
+        <div v-for="sample in filteredSamples" :key="sample.id">
             <TextCard :key="sample.id" :id="sample.id" :text="sample.text" :category="sample.category" />
         </div>
     </div>
