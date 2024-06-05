f29159e3807e47ac14812387e396eef938689d63
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-05 06:18:44 +0600

    increase scale while tapping

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -20,9 +20,10 @@ const attrs = useAttrs()
     transition: {
       type: 'spring'
     }
-  }" :duration="600" :class="cn(
-    'font-bold py-2 px-4 rounded',
-    attrs.class ? (attrs.class as ClassValue[]) : [], variant === 'solid' ? 'bg-gradient-to-tr from-primary to-secondary text-white' : '', variant === 'ghost' ? 'dark:hover:bg-slate-700 hover:bg-slate-300' : '', variant === 'outline' ? 'border border-primary hover:border-secondary' : '')">
+  }" :tapped="{ scale: 1.02 }" :duration="600"
+    :class="cn(
+      'font-bold py-2 px-4 rounded',
+      attrs.class ? (attrs.class as ClassValue[]) : [], variant === 'solid' ? 'bg-gradient-to-tr from-primary to-secondary text-white' : '', variant === 'ghost' ? 'dark:hover:bg-slate-700 hover:bg-slate-300' : '', variant === 'outline' ? 'border border-primary hover:border-secondary' : '')">
     <slot />
   </button>
 </template>
