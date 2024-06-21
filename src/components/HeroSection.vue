9e61a3875db6c40514e4c2cdf192cd4e5c0dd078
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-21 12:26:17 +0600

    style: add icon

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,17 +1,21 @@
 <script lang="ts" setup>
 import Button from '@/components/Button.vue'
+import { AudioLines } from 'lucide-vue-next';
+import { MicVocal } from 'lucide-vue-next';
 </script>
 
 <template>
     <div class="w-full grid place-items-center py-12">
         <h1 class="mb-4 text-3xl font-bold g-text">Pratice to learn Verbal Communicaiton!</h1>
         <div class="flex items-center gap-3">
-            <Button variant="outline">
+            <Button class="flex items-center gap-2" variant="outline">
+                <MicVocal />
                 Practice sentences
             </Button>
-            
+
             <router-link to='/vocabulary'>
-                <Button variant="outline">
+                <Button class="flex items-center gap-2" variant="outline">
+                    <AudioLines />
                     Practice vocabulary
                 </Button>
             </router-link>
