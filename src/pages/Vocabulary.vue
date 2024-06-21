64e59e450232362c84acf339781c14161988ebf4
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-21 11:55:37 +0600

    style: add speaker btn

--- src/pages/Vocabulary.vue
+++ src/pages/Vocabulary.vue
@@ -1,17 +1,21 @@
 <script lang="ts" setup>
 import { vocabulary } from '@/lib/vocabulary';
+import { Volume2 } from 'lucide-vue-next';
+import Button from '@/components/Button.vue'
 </script>
 
 <template>
     <h1 class="text-3xl font-bold text-center mb-4 g-text">Practice vocabulary</h1>
 
     <div class="flex gap-2 flex-col">
-        <div class="flex items-center p-3 gap-2 rounded border" v-for="(item, index) in vocabulary" :key="index">
+        <div class="flex items-center justify-between p-3 gap-2 rounded border" v-for="(item, index) in vocabulary" :key="index">
             <div class="flex items-center gap-2">
                 <div>{{ item.en }}</div>
                 <div>{{ item.bn }}</div>
             </div>
-            <div></div>
+            <div>
+                <Button variant="ghost"><Volume2 /></Button>
+            </div>
         </div>
     </div>
 </template>
