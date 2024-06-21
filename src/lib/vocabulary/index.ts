a51df51f41d71bfdea752453c89003d2d0a32c9e
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-21 20:00:11 +0600

    feat: concat battle vocabulary

--- src/lib/vocabulary/index.ts
+++ src/lib/vocabulary/index.ts
@@ -1,6 +1,7 @@
 import { physicsVocabularyArray } from "./physics";
 import { historyVocabularyArray } from "./history";
+import { battleVocabularyArray } from "./battle";
 
 import { Vocabulary } from "./types";
 
-export const vocabulary : Vocabulary[] = [...physicsVocabularyArray,...historyVocabularyArray];
+export const vocabulary : Vocabulary[] = [...physicsVocabularyArray,...historyVocabularyArray,...battleVocabularyArray];
