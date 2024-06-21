acfc005da6bee9685eb68e9bc2fe29a2b9a71445
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-21 12:27:33 +0600

    style: add primary color in outline tbn

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -23,7 +23,7 @@ const attrs = useAttrs()
   }" :tapped="{ scale: 1.02 }" :duration="600"
     :class="cn(
       'font-bold py-2 px-4 rounded',
-      attrs.class ? (attrs.class as ClassValue[]) : [], variant === 'solid' ? 'bg-gradient-to-tr from-primary to-secondary text-white' : '', variant === 'ghost' ? 'dark:hover:bg-slate-700 hover:bg-slate-300' : '', variant === 'outline' ? 'border border-primary hover:border-secondary' : '')">
+      attrs.class ? (attrs.class as ClassValue[]) : [], variant === 'solid' ? 'bg-gradient-to-tr from-primary to-secondary text-white' : '', variant === 'ghost' ? 'dark:hover:bg-slate-700 hover:bg-slate-300' : '', variant === 'outline' ? 'border text-primary hover:text-secondary border-primary hover:border-secondary' : '')">
     <slot />
   </button>
 </template>
