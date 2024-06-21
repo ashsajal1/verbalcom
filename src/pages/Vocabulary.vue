e51caae4876418c1c4f6d3c66dd072e2350fc264
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-21 11:48:39 +0600

    feat: show vocabulary

--- src/pages/Vocabulary.vue
+++ src/pages/Vocabulary.vue
@@ -1,7 +1,17 @@
 <script lang="ts" setup>
-
+import { vocabulary } from '@/lib/vocabulary';
 </script>
 
 <template>
-    <h1>Practice vocabulary</h1>
+    <h1 class="text-3xl font-bold text-center mb-4 g-text">Practice vocabulary</h1>
+
+    <div class="flex gap-2 flex-col">
+        <div class="flex items-center p-3 gap-2 rounded border" v-for="(item, index) in vocabulary" :key="index">
+            <div class="flex items-center gap-2">
+                <div>{{ item.en }}</div>
+                <div>{{ item.bn }}</div>
+            </div>
+            <div></div>
+        </div>
+    </div>
 </template>
