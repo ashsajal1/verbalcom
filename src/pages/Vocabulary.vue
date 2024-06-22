fb29ff7ae38b59300110c17ddee0c56b824ba31a
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-22 15:58:37 +0600

    feat: add search word option

--- src/pages/Vocabulary.vue
+++ src/pages/Vocabulary.vue
@@ -3,6 +3,24 @@ import { vocabulary } from '@/lib/vocabulary';
 import { Volume2 } from 'lucide-vue-next';
 import Button from '@/components/Button.vue'
 import { useSpeechSynthesis } from '@vueuse/core'
+import { computed, ref, watch } from 'vue';
+
+const searchedWord = ref('')
+const words = computed(() => {
+    if (searchedWord.value === '') {
+        return vocabulary;
+    }
+    return vocabulary.filter(word =>
+        word.en.includes(searchedWord.value) || word.bn.includes(searchedWord.value)
+    );
+});
+
+watch(
+    words,
+    async (newWords) => {
+        console.log("Words are : ", newWords)
+    },
+);
 
 const playWord = (word: string) => {
     const {
@@ -31,8 +49,24 @@ const playWordWithoutAlert = (word: string) => {
 <template>
     <h1 class="text-3xl font-bold text-center mb-4 g-text">Practice vocabulary</h1>
 
+    <div class="pt-3 pb-6 flex flex-col md:flex-row w-full">
+        <div class="flex items-center gap-2">
+            <input v-model="searchedWord" class="p-2 border w-full rounded" placeholder="Search word" type="text">
+            <Button>Search</Button>
+        </div>
+    </div>
+
+    <div class="rounded flex items-center justify-between border text-lg font-bold p-4 shadow-sm" v-if="words.length === 0">
+        <div>
+            <span>No word found called </span>
+            <span class="text-secondary">{{ searchedWord }}</span>
+        </div>
+
+        <Button variant="outline" @click="searchedWord = ''">Reset</Button>
+    </div>
+
     <div class="flex gap-2 flex-col">
-        <div class="flex items-center justify-between p-3 gap-2 rounded border" v-for="(item, index) in vocabulary"
+        <div class="flex items-center justify-between p-3 gap-2 rounded border" v-for="(item, index) in words"
             :key="index">
             <div class="flex items-center gap-2">
                 <Button @click="playWordWithoutAlert(`${item.en}`)" variant="ghost">
