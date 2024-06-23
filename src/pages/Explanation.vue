64ca8aadb1c40681594f217f19bb5fe33fd6eab4
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-23 11:31:20 +0600

    feat: show explanation

--- src/pages/Explanation.vue
+++ src/pages/Explanation.vue
@@ -1,7 +1,37 @@
 <script lang="ts" setup>
+import { vocabulary } from '@/lib/vocabulary';
+import { useUrlSearchParams } from '@vueuse/core'
 
+const searchedWord = useUrlSearchParams('history').word;
+const wordExplanation = vocabulary.filter(word => word.en === searchedWord)[0]
+console.log(wordExplanation)
 </script>
 
+<!-- en: string;
+bn: string;
+category: string;
+explanation?: string;
+example?: string; -->
+
 <template>
-    <h1>Explanation page</h1>
+    <div>
+        <p v-if="searchedWord">Showing explanation for word <span class="text-primary">{{ searchedWord }}</span></p>
+
+        <div>
+            <p>Meaning in Bengali : {{ wordExplanation?.bn }}</p>
+
+            <p v-if="wordExplanation.explanation">
+                Explanation :
+                <span>{{ wordExplanation.explanation }}</span>
+            </p>
+            <p v-if="wordExplanation.example">
+                Example usage :
+                <span>{{ wordExplanation.example }}</span>
+            </p>
+            <p v-if="wordExplanation.category">
+                Word category :
+                <span>{{ wordExplanation.category }} realated word.</span>
+            </p>
+        </div>
+    </div>
 </template>
