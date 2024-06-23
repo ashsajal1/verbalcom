5c4f208981b0306ac19ac4add85127cd39569d67
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-23 10:53:30 +0600

    style: light text

--- src/components/TextCard.vue
+++ src/components/TextCard.vue
@@ -12,7 +12,7 @@ const handleClick = () => {
 
 <template>
   <div v-motion-roll-top :duration="600" class="flex flex-col border dark:border-gray-700 shadow p-4 rounded">
-    <p @click="handleClick" class="font-extralight text-2xl">{{ text }}</p>
+    <p @click="handleClick" class="font-light text-2xl">{{ text }}</p>
     <p class="font-bold text-sm">{{ category }}</p>
     <Button class="mt-2 hover:to-primary hover:from-primary" @click="handleClick">Practice</Button>
   </div>
