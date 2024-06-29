084cb26d2cdd98084e377c4857e8d2315903f48b
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-29 20:22:13 +0600

    resource: add business vocab

--- src/lib/vocabulary/index.ts
+++ src/lib/vocabulary/index.ts
@@ -3,5 +3,6 @@ import { historyVocabularyArray } from "./history";
 import { battleVocabularyArray } from "./battle";
 
 import { Vocabulary } from "./types";
+import { businessVocabularyArray } from "./business";
 
-export const vocabulary : Vocabulary[] = [...physicsVocabularyArray,...historyVocabularyArray,...battleVocabularyArray];
+export const vocabulary : Vocabulary[] = [...physicsVocabularyArray,...historyVocabularyArray,...battleVocabularyArray, ...businessVocabularyArray];
